96f6feeb6213facf01af795fba002274f034ad57
MThibault <tmillant@gmail.com>
2016-06-03 14:06:00 +0200

    Ajout des exercices du cours 3 sur les Séquences

---
+++ Cours3_Séquences/sequences.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.5
+
+print ("**********Exercice 1**********")
+ma_liste = [1, 2, 3]
+print (ma_liste)
+ma_liste.remove(1)
+ma_liste.append(3)
+print (ma_liste)
+
+print ("**********Exercice 2**********")
+mon_dico = {"AS": 11, "10": 10, "ROI": 4, "DAME": 3}
+print (mon_dico)
+
+print ("**********Exercice 3**********")
+mon_tuple = (1, 2, 3)
+ma_liste = list ()
+i = 0
+while i < len (mon_tuple):
+	ma_liste.append(mon_tuple[i])
+	i += 1
+print (ma_liste)
+ma_liste.remove(1)
+ma_liste.append(3)
+mon_tuple = ma_liste
+print (mon_tuple)
